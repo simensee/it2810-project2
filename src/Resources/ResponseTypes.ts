fe026f7a6940c8c1e30174dfaccd05cce1ea5ef7
studBrage <bragehe99@gmail.com>
2022-09-19 17:53:21 +0200

    feat: create interface for user and event

---
+++ src/Resources/ResponseTypes.ts
@@ -0,0 +1,12 @@
+
+
+export interface User {
+    name: string;
+    username: string;
+}
+
+export interface Action {
+    action_name: string;
+    author_username: string;
+    created_at: string;
+}
