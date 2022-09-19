2bb6475283ce9f3bbb27775b61c8fa96914ef4d2
studBrage <bragehe99@gmail.com>
2022-09-19 17:53:42 +0200

    feat: setup queryselector

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FetchEvents, FetchUsers } from './Resources/APIWrapper';
 
 function App() {
+
+  FetchUsers();
+  FetchEvents();
+  
   return (
     <div className="App">
       <header className="App-header">
