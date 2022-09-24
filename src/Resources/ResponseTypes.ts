d4723c664e73aebb3545523779b532ca8e4c9859
simensee <simensee@gmail.com>
2022-09-24 12:45:40 +0200

    make interface for commit
    Co-authored-by: Pia <piaas@stud.ntnu.no>

--- src/Resources/ResponseTypes.ts
+++ src/Resources/ResponseTypes.ts
@@ -49,3 +49,20 @@ export interface PushData {
     commitTitle: string;
     refCount:    null;
 }
+
+export interface Commit {
+    id:              string;
+    short_id:        string;
+    created_at:      Date;
+    parent_ids:      string[];
+    title:           string;
+    message:         string;
+    author_name:     string;
+    author_email:    string;
+    authored_date:   Date;
+    committer_name:  string;
+    committer_email: string;
+    committed_date:  Date;
+    trailers:        Trailers;
+    web_url:         string;
+}
