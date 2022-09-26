8e452918619cd3aa4c3aa6c78699ab62578cfab7
brageher <brageher@stud.ntnu.no>
2022-09-26 15:49:40 +0200

    feat: make sidebar with styling and routing

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,18 +1,55 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { AppRoutes } from '../Router/AppRoutes'
+import NavButton from './NavButton'
+import ArticleIcon from '@mui/icons-material/Article';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
 
 const Sidebar = () => {
   return (
-        <div className='sidebar'>
-            <h1>Inital router</h1>
-            <ul className="header">
-                <li><Link to={AppRoutes.readme}>Readme</Link></li>
-                <li><Link to={AppRoutes.usersPage}>Users</Link></li>
-                <li><Link to={AppRoutes.progressPage}>Progress</Link></li>
-                <li><Link to={AppRoutes.brancesPage}>Branches</Link></li>
-            </ul>
+    <div className='flex'>
+      <aside className='w-64 max-h-screen'>
+        <div className='w-64 fixed left-0 top-0 h-screen bg-slate-700 py-10 px-5 min-h-screen'>
+          <div className='m-4 text-5xl text-gray-100'>
+            <h1>Nettside</h1>
+          </div>
+          <ul className='space-y-2'>
+            <li >
+              <NavButton
+                route={AppRoutes.readme}
+              >
+                <ArticleIcon />
+                <span>Readme</span>
+              </NavButton>
+            </li>
+            <li>
+              <NavButton
+                route={AppRoutes.usersPage}
+              >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                <span>Users</span>
+              </NavButton>
+            </li>
+            <li>
+              <NavButton
+                route={AppRoutes.progressPage}
+              >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"></path></svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Progress</span>
+              </NavButton>
+            </li>
+            <li>
+              <NavButton
+                route={AppRoutes.brancesPage}
+              >
+                <AccountTreeIcon />
+                <span className="flex-1 ml-3 whitespace-nowrap">Branches</span>
+              </NavButton>
+            </li>
+          </ul>
         </div>
+      </aside>
+    </div>
   )
 }
 
