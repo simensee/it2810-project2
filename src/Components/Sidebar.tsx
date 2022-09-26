362515f1d32d862182c99e1b1d6ccd3a0b2fa375
brageher <brageher@stud.ntnu.no>
2022-09-26 13:21:32 +0200

    fix: styling

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -7,14 +7,14 @@ const Sidebar = () => {
   return (
     <div className="flex">
     <aside className="w-64 max-h-screen" aria-label="Sidebar" >
-      <div className="w-44 fixed left-0 top-0 h-screen bg-slate-700 p-10 overflow-y-auto py-4 px-3 bg-gray-50 dark:bg-gray-800 min-h-screen">
+      <div className="w-60 fixed left-0 top-0 h-screen bg-slate-700 overflow-y-auto py-12 px-8 bg-gray-50 dark:bg-gray-800 min-h-screen">
         <a href="/start" className="flex items-center pl-2.5 mb-5">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" />
+        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" /> */}
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">StartPage</span>
         </a>
         <ul className="space-y-2">
           <li>
-            <a href="/users" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <a href="/users" className="flex items-center justify-center space-x-4 px-4 py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
               <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
             </a>
@@ -29,7 +29,6 @@ const Sidebar = () => {
             <a href="branches" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span className="flex-1 ml-3 whitespace-nowrap">Branches</span>
-
             </a>
           </li>
         </ul>
