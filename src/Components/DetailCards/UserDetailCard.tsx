f4d0c573192c0f66360643c19ae80b7002473a2c
brageher <brageher@stud.ntnu.no>
2022-09-26 18:25:55 +0200

    feat: display bot name as bot

--- src/Components/DetailCards/UserDetailCard.tsx
+++ src/Components/DetailCards/UserDetailCard.tsx
@@ -23,20 +23,30 @@ const UserDetailCard = ({ focusUser }: UserDetailCardProps) => {
         return n;
     }
 
+
+    function isBot(name: string): boolean {
+        return name[0] === '*';
+    }
+
     const totalCommits: number = GetUserTotalCommits(focusUser)
 
     return (
         <div className='flex items-center flex-col gap-4 pt-6 bg-elem-bg h-full'>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 items-center'>
                 <img src={focusUser.avatar_url} alt="img not found" className='w-52 h-52 rounded-full' />
-                <div className='flex flex-col items-center'>
-                    <span>{focusUser.name}</span>
+                <div className='h-30 flex flex-col items-center  p-2 overflow-auto'>
+                    <span className='text-center text-2xl overflow-wrap'>{( isBot(focusUser.name) ? 'Project bot' :focusUser.name)}</span>
                     <span>@{focusUser.username}</span>
 
                 </div>
             </div>
-            <div className='flex flex-col'>
-                <span>{totalCommits}</span>
+            <div className='flex flex-col items-center'>
+                <span>Total commits: {totalCommits}</span>
+                <p className='p-4'>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                </p>
             </div>
         </div>
     )
