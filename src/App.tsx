60b4d54fdd51c03607540e971f27cb0714916816
brageher <brageher@stud.ntnu.no>
2022-09-28 15:26:19 +0200

    fix: actually do fetch

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   const [loadComplete, setLoadComplete] = useState(false);
 
   useEffect(() => {
-    // setup().then(() => setLoadComplete(true)).then(() => console.log(ctx.usersData));
+    setup().then(() => setLoadComplete(true)).then(() => console.log(ctx.usersData));
   }, []);
 
   // console.log(FetchUsers());
