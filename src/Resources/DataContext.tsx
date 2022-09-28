4da23a740bf10f09e31f328f5487fbc8d37e8639
brageher <brageher@stud.ntnu.no>
2022-09-28 15:09:05 +0200

    feat: add functions for authentication

--- src/Resources/DataContext.tsx
+++ src/Resources/DataContext.tsx
@@ -5,6 +5,9 @@ import { Commit, User } from './ResponseTypes';
 interface DataContextProps {
     usersData: User[],
     commitData: Commit[],
+    isAuthorized: boolean,
+    setIsAuthorized: Function,
+    setCredentials: Function,
     fetchUsers: Function,
     fetchCommits: Function,
 }
@@ -12,6 +15,9 @@ interface DataContextProps {
 export const DataContext = createContext<DataContextProps>({
     usersData: [],
     commitData: [],
+    isAuthorized: false,
+    setIsAuthorized: () => null,
+    setCredentials: () => null,
     fetchUsers: () => null,
     fetchCommits: () => null,
 });
@@ -21,14 +27,33 @@ export interface LayoutProps {
 }
 
 export const DataContextProvider = (props: LayoutProps) => {
-    const stringUrl: string = 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/17475/';
+    const baseUrl: string = 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/17475/';
     const APIToken: string = 'glpat-cygbLETJKv1wXaNyMtXS';
 
+    let repoId: string = '';
+    let ApiToken: string = '';
+    
     let usersData: User[] = [];
     let commitData: Commit[] = [];
+    
+    const [isAuthorized, setAuthorized] = useState(false);
+
+    if (sessionStorage.getItem('isAuth') === 'true' && !isAuthorized){
+        setAuthorized(true);
+    }
+
+    const setCredentials = (id: string, token: string) => {
+        repoId = id;
+        ApiToken = token;
+    }
+
+    const setIsAuthorized = async (val: boolean) => {
+        setAuthorized(val);
+        sessionStorage.setItem('isAuth', val ? 'true' : 'false');
+    }
 
     const fetchUsers = async () => {
-        const usersUrl = stringUrl.concat('users');
+        const usersUrl = baseUrl.concat('users');
         let fetchUsersUrl: URL = new URL(usersUrl);
 
         await fetch(fetchUsersUrl, {
@@ -44,7 +69,7 @@ export const DataContextProvider = (props: LayoutProps) => {
     }
 
     const fetchCommits = async () => {
-        const commitUrl = stringUrl.concat('repository/commits');
+        const commitUrl = baseUrl.concat('repository/commits');
         let fetchCommitUrl: URL = new URL(commitUrl);
 
         await fetch(fetchCommitUrl, {
@@ -56,12 +81,16 @@ export const DataContextProvider = (props: LayoutProps) => {
         }).then(res => res.json()).then((res) => {
             const commitResponse: Commit[] = res;
             commitData.push(...commitResponse);
+
         });
     }
 
     const value: DataContextProps = {
         usersData,
         commitData,
+        isAuthorized,
+        setIsAuthorized,
+        setCredentials,
         fetchUsers,
         fetchCommits,
     };
