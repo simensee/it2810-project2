87fab4740a6766063e11f0472d41f962e74a8233
lise jakobsen <lisejak@stud.ntnu.no>
2022-09-28 10:32:25 +0200

    feat: more design to UserCard

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -13,17 +13,15 @@ const UserCard = ({user}: UserCardProps) => {
     console.log(user.web_url);
 
     return (
-        <div className="text-white bg-indigo-100 w-24 rounded-xl">
-            <div className="">
-                <h2 className="">{user.name}</h2>
+        <div className="max-witdh-xs w-52 h-48 text-white bg-indigo-100 rounded-xl p-5 pt-6">
+            <div className="space-y-3">
+                <h2 className="text-center">{user.name}</h2>
                 <img 
-                className="rounded-full"
+                className="rounded-full mx-auto"
                 src={user.avatar_url}
                 alt="avatar_url">
                 </img>
-            </div>
-            <div className="">
-                <p className="">{user.username}</p>
+                <p className="text-center">{user.username}</p>
             </div>
         </div>
     )
