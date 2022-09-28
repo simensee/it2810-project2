e200ab344c7932f542f6f7f1e377e471d35ace02
brageher <brageher@stud.ntnu.no>
2022-09-28 15:07:16 +0200

    setup: implement login route in app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavRouter from './Components/Router/NavRouter';
 import { DataContext } from './Resources/DataContext';
 import { classicNameResolver } from 'typescript';
 import classnames from 'tailwindcss-classnames';
+import LoginPage from './Components/Pages/LoginPage';
 
 function App() {
   const ctx = useContext(DataContext);
@@ -15,31 +16,36 @@ function App() {
     await ctx.fetchCommits();
   }
 
+  const [loadComplete, setLoadComplete] = useState(false);
 
-
-  useEffect(() => {  
-    setup();
+  useEffect(() => {
+    // setup().then(() => setLoadComplete(true)).then(() => console.log(ctx.usersData));
   }, []);
-
 
   // console.log(FetchUsers());
 
   // console.log(resp[0]);s
-  
-  
+
+
   // resp?.map(u => console.log(u.name))
   // console.log(FetchEvents());
 
   // ctx.setVal('Hallo');
-  
+
   return (
     <>
-    <nav>
-      <Sidebar/>
-    </nav>
-    <div className='ml-64 pl-4 pb-8 lg:pt-12 px-4 h-full'>
+      {/* <LoginPage/> */}
       <NavRouter/>
-    </div>
+      {/* <nav>
+        <Sidebar />
+      </nav>
+      <div className='ml-64 pl-4 pb-8 lg:pt-12 px-4 h-full'>
+        {loadComplete ?
+          <span>done</span>
+          : <span>Loading data</span>
+        }
+        <NavRouter/>
+      </div> */}
     </>
   );
 }
