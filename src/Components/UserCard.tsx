a70d7911b33e7f1b865d8f6faf24e56166eefacd
lise jakobsen <lisejak@stud.ntnu.no>
2022-09-28 15:41:59 +0200

    feat: more design

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -13,15 +13,17 @@ const UserCard = ({user}: UserCardProps) => {
     console.log(user.web_url);
 
     return (
-        <div className="max-witdh-xs w-52 h-48 text-white bg-indigo-100 rounded-xl p-5 pt-6">
-            <div className="space-y-3">
-                <h2 className="text-center">{user.name}</h2>
+        <div className="max-witdh-xs w-48 h-52 text-white bg-indigo-100 rounded-lg p-5 pt-6">
+            <div className="space-y-2">
                 <img 
                 className="rounded-full mx-auto"
                 src={user.avatar_url}
                 alt="avatar_url">
                 </img>
-                <p className="text-center">{user.username}</p>
+                <div className="">
+                    <h2 className="text-center text-gray-400 font-medium">{user.name}</h2>
+                    <p className="text-center">@{user.username}</p>
+                </div>
             </div>
         </div>
     )
