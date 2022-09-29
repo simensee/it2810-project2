a8809f82789730fe241b324ec3f593ea63c6bfe0
simensee <simensee@gmail.com>
2022-09-29 16:55:25 +0200

    feat: added issue api in ctx

--- src/Resources/DataContext.tsx
+++ src/Resources/DataContext.tsx
@@ -1,29 +1,34 @@
 import React, { createContext, useState, ReactNode } from 'react'
-import { Commit, User, MergeRequest } from './ResponseTypes';
+import { Commit, User, MergeRequest, Issue } from './ResponseTypes';
 
 
 interface DataContextProps {
     usersData: User[],
     commitData: Commit[],
     mergeData: MergeRequest[],
+    issueData: Issue[],
     isAuthorized: boolean,
     setIsAuthorized: Function,
     setCredentials: Function,
     fetchUsers: Function,
     fetchCommits: Function,
     fetchMergeRequests: Function,
+    fetchIssues: Function,
+
 }
 
 export const DataContext = createContext<DataContextProps>({
     usersData: [],
     commitData: [],
     mergeData: [],
+    issueData: [],
     isAuthorized: false,
     setIsAuthorized: () => null,
     setCredentials: () => null,
     fetchUsers: () => null,
     fetchCommits: () => null,
     fetchMergeRequests: () => null,
+    fetchIssues: () => null,
 });
 
 export interface LayoutProps {
@@ -40,6 +45,7 @@ export const DataContextProvider = (props: LayoutProps) => {
     let usersData: User[] = [];
     let commitData: Commit[] = [];
     let mergeData: MergeRequest[] = [];
+    let issueData: Issue[] = [];
     
     // Login is set to true for easier development
     const [isAuthorized, setAuthorized] = useState(true);
@@ -119,6 +125,28 @@ export const DataContextProvider = (props: LayoutProps) => {
         }
     }
 
+    const fetchIssues = async () => {
+        const issueUrl = baseUrl.concat('issues');
+        let fetchIssueUrl: URL = new URL(issueUrl + "?pagination=keyset");
+        let page: number = 1;
+        let finished: boolean = false;
+        while (!finished) {
+        await fetch(fetchIssueUrl + "&page=" + page, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + APIToken,
+            })
+        }).then(res => res.json()).then((res) => {
+            if (res.length < 20) finished = true;
+            console.log(res.length);
+            const issueResponse: Issue[] = res;
+            issueData.push(...issueResponse);
+            page++;
+        });
+        }
+    }
+
 
     
 
@@ -126,12 +154,14 @@ export const DataContextProvider = (props: LayoutProps) => {
         usersData,
         commitData,
         mergeData,
+        issueData,
         isAuthorized,
         setIsAuthorized,
         setCredentials,
         fetchUsers,
         fetchCommits,
         fetchMergeRequests,
+        fetchIssues,
     };
 
     return <DataContext.Provider value={value}>{props.children}</DataContext.Provider>
