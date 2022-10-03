57b1b2e4d50dea16e5a77dbc2dadba7532525cd3
lise jakobsen <lisejak@stud.ntnu.no>
2022-10-03 16:22:23 +0200

    feat: design card - missing correct color

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -13,16 +13,16 @@ const UserCard = ({user}: UserCardProps) => {
     console.log(user.web_url);
 
     return (
-        <div className="max-witdh-xs w-48 h-52 text-white bg-indigo-100 rounded-lg p-5 pt-6">
+        <div className="max-witdh-xs w-48 h-56 bg-indigo-100 p-5 pt-6">
             <div className="space-y-2">
                 <img 
-                className="rounded-full mx-auto"
+                className="rounded-full mx-auto w-24 h-24"
                 src={user.avatar_url}
                 alt="avatar_url">
                 </img>
                 <div className="">
-                    <h2 className="text-center text-gray-400 font-medium">{user.name}</h2>
-                    <p className="text-center">@{user.username}</p>
+                    <h2 className="text-lg text-center text-black-400 font-medium">{user.name}</h2>
+                    <p className="text-sm text-center text-gray-400">@{user.username}</p>
                 </div>
             </div>
         </div>
