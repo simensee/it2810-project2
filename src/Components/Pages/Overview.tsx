a2ca8ff96c67434d57c2b2279a6c119848b7daf6
lise jakobsen <lisejak@stud.ntnu.no>
2022-10-03 21:27:55 +0200

    feat: made line graph and pie

--- src/Components/Pages/Overview.tsx
+++ src/Components/Pages/Overview.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { DataContext } from '../../Resources/DataContext';
-import { Commit, MergeRequest } from '../../Resources/ResponseTypes';
+import { Commit, User } from '../../Resources/ResponseTypes';
 import { Chart as ChartJS, registerables } from 'chart.js';
-import { Chart, Line } from 'react-chartjs-2'
+import { Chart, Line, Pie } from 'react-chartjs-2'
 ChartJS.register(...registerables);
 // import Graph from '../Graph';
 
@@ -10,26 +10,88 @@ ChartJS.register(...registerables);
 const Overview = () => {
     const ctx = useContext(DataContext);
     const commitList: Commit[] = ctx.commitData;
+    const userList: User[] = ctx.usersData;
+    // console.log('commitList length', commitList.length);
 
-    const [commitData, setCommitData] = useState({
-        labels: commitList.reverse().map((date) => date.committed_date.split('T')[0]),
+    const dateList: string[] = [""];
+
+    const dates = commitList.reverse().map((commit) => {
+        if (!(dateList.includes(commit.committed_date.split('T')[0])) && (dateList.length != 0)) {
+            dateList.push(commit.committed_date.split('T')[0]);
+        }
+    })
+
+    // console.log('dateList', dateList);
+
+    const dateCount = dateList.map((date) => {
+        let count = 0;
+        commitList.reverse().forEach((commit) => {
+            if (commit.committed_date.split('T')[0] === date) {
+                count++;
+            }
+        })
+        return count;
+    })
+
+    const usernames = userList.map((user) => {
+        return user.name;
+    })
+
+    const userCommits = userList.map((user) => {
+        let count = 0;
+        commitList.forEach((commit) => {
+            if (commit.author_name === user.name) {
+                count++;
+            }
+        })
+        return count;
+    })
+    
+    const commitData = {
+        labels: dateList,
         datasets: [{
             label: 'Commits',
-            data: commitList.map((date) => date.title),
+            data: dateCount.map((count) => count),
             borderColor: 'rgb(53, 162, 235)',
             backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            fill: true,
+            
         }],
         borderColor: 'black',
         borderwidth: 1,
-    });
+    };
 
+    var options = {
+        scales: {
+            y: {
+                beginAtZero: true,
+                min: 0,
+                max: 35,
+            }
+        }
+    }
     console.log(commitData);
 
+    const userData = {
+        labels: usernames,
+        datasets: [{
+            label: 'Commits',
+            data: userCommits.map((count) => count),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            fill: true,
+        }]
+    }
+
+
     return (
-        <div className='max-w-screen-lg'>
-            <h1>Heeeeiii</h1>
-            <Line data={commitData} />
-            {/* <Graph commitData={commitData}/> */}
+        <div>
+            <div className='max-w-screen-lg'>
+                <Line data={commitData} options={options}/>
+            </div>
+            <div className='w-4/12'>
+                <Pie data={userData}/>
+            </div>
         </div>
     )
 }
--- src/Components/Pages/UsersPage.tsx
+++ src/Components/Pages/UsersPage.tsx
@@ -14,8 +14,8 @@ const UsersPage = () => {
   const ctx = useContext(DataContext);
   const userList: User[] = ctx.usersData;
 
+  
  
-
   return (
     <div className='grid grid-cols-3 gap-4'>
       <div className='col-span-2 grid grid-cols-3 gap-4'>
