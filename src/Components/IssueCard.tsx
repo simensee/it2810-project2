79d491becf77afad2ee2a2dd764e4ed22de86c7b
brageher <brageher@stud.ntnu.no>
2022-10-03 19:55:16 +0200

    fix: style card

--- src/Components/IssueCard.tsx
+++ src/Components/IssueCard.tsx
@@ -5,22 +5,26 @@ import IssueLabel from './IssueLabel';
 type IssueCardProps = {
     issue: Issue;
     handleOnClick: (i: Issue) => void;
+    selected: boolean;
 }
 
-const IssueCard = ({ issue, handleOnClick }: IssueCardProps) => {
+const IssueCard = ({ issue, handleOnClick, selected }: IssueCardProps) => {
+
+    const date: string = issue.created_at!.split('T')[0] ?? '';
     
     return (
         <div 
-        className='flex flex-col outline rounded-md p-2 h-28 justify-between'
+        className='flex flex-col gap-2 bg-stone-200 hover:bg-stone-100 rounded-md py-3 px-3 justify-between hover:cursor-pointer'
         onClick={() => handleOnClick(issue)}
+        style={{backgroundColor: selected ? '#f5f5f4' : ''}}
         >
-            <span>{issue.title}</span>
-            <div className='flex gap-2'>
+            <span className='pb-2 flex text-md font-semibold'>{issue.title}</span>
+            <div className='flex flex-wrap gap-2'>
                 {issue.labels?.map((label) => {
                     return <IssueLabel label={label}/>
                 })}
             </div>
-            <span>{'none'}</span>
+            <span className='text-black/50'>{date}</span>
         </div>
     )
 }
