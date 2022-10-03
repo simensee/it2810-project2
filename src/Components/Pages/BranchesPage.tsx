a7d874242a6e96195396b99d6b4568b235bee839
lise jakobsen <lisejak@stud.ntnu.no>
2022-10-03 12:54:37 +0200

    feat: showing the searched branches

--- src/Components/Pages/BranchesPage.tsx
+++ src/Components/Pages/BranchesPage.tsx
@@ -7,51 +7,33 @@ const BranchesPage = () => {
   const ctx = useContext(DataContext);
   const branchList: Branch[] = ctx.branchesData;
 
-  
-  // const SearchBar = () => {
-  //   const [searchTerm, setSearchTerm] = useState("");
-  //   const [searchResults, setSearchResults] = useState([]);
-  //   const searchItems = (searchTerm: string) => {
-      // setSearchTerm(searchTerm);
-      // if (searchTerm !== "") {
-      //   const newBranchList = branchList.filter((branch) => {
-      //     return Object.values(branch).join(" ").toLowerCase().includes(searchTerm.toLowerCase());
-      //   });
-      //   setSearchResults(newBranchList);
-      // } else {
-      //   setSearchResults(branchList);
-      // }
-    // };
-    
-  //   return (
-  //     <div className='flex flex-row justify-left'>
-  //       <input type='text' className='border-2 border-black rounded-md p-2' placeholder='Search for a branch' />
-  //       <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' 
-  //       onClick={() => console.log("Dette er branchList", branchList)}>Search</button>
-  //     </div>
-  //   )
-  // }
-
   const [searchInput, setSearchInput] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState(branchList);
 
-  let searchItems = (searchInput: string) => {
+  const searchItems = (searchInput: string) => {
+    console.log(searchInput);
     setSearchInput(searchInput);
-    if (searchInput !== "") {
-      const newBranchList = branchList.filter((branch) => {
-        return Object.values(branch).join(" ").toLowerCase().includes(searchInput.toLowerCase());
-      });
-      setSearchResults(newBranchList);
-    } else {
-      setSearchResults(branchList);
+    const results = branchList.filter((branch) => {
+      console.log(branch.name);
+      if (searchInput === "") {
+        return branch;
+      }
+      return branch.name.toLowerCase().includes(searchInput.toLowerCase());
+    });
+    setSearchResults(results);
     }
-  };
+    
 
   return (
     <div>
         <h1>Branches</h1>
         <div className='flex flex-row justify-left'>
-          <input type='text' className='border-2 border-black rounded-md p-2' placeholder='Search for a branch' />
+          <input 
+          type='text' 
+          className='border-2 border-black rounded-md p-2' 
+          placeholder='Search for a branch'
+          onChange={(e) => searchItems(e.target.value)}
+          />
           <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' 
             onClick={() => console.log("Dette er branchList", branchList)}>Search</button>
           {/* In the div below: Get the branch cards that you have searched for */}
@@ -60,7 +42,7 @@ const BranchesPage = () => {
           </div>
         </div>
         <div>
-          {branchList.map((b) => {
+          {searchResults.map((b) => {
             return <div key={b.id} className='p-8 hover:outline flex pointer-events-auto'>
               {b.name}
             </div>
