13f1d2875aad7baf15fd1183a8b4022d213e996f
brageher <brageher@stud.ntnu.no>
2022-10-04 12:13:24 +0200

    feat: add component for tab buttons

---
+++ src/Components/ProgressPageComponents/TabButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type TabButtonProps = {
+    label: string;
+    selected: boolean;
+    handleCLick: (l: string) => void;
+}
+
+const TabButton = ({label, selected, handleCLick}: TabButtonProps) => {
+  return (
+    <div
+    className="border-b-2 hover:border-blue-500 hover:cursor-pointer"
+    onClick={() => handleCLick(label)}
+    style={{borderBottomColor: selected ? '#60a5fa' : ''}}>
+        <span>{label}</span>
+    </div>
+  )
+}
+
+export default TabButton
