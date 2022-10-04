0fb6f6aef1836967c5cc16a191376ad40e38d8a7
brageher <brageher@stud.ntnu.no>
2022-10-04 12:13:00 +0200

    fix: implement new compnents

--- src/Components/ProgressPageComponents/MergeRQList.tsx
+++ src/Components/ProgressPageComponents/MergeRQList.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MergeRequest } from '../../Resources/ResponseTypes'
+import MergeDetailCard from '../DetailCards/MergeDetailCard';
+import MergeRQDetails from './MergeRQDetails';
+import MergeRQListTile from './MergeRQListTile';
 
 type MergeRequestProps = {
     mergeRequestList: MergeRequest[];
 }
 
 const MergeRQList = ({ mergeRequestList }: MergeRequestProps) => {
+
+    const [focusMerge, setFocusMerge] = useState<MergeRequest>({ title: 'empty' });
+
+    useEffect(() => {
+        const prevMergeRq: MergeRequest = mergeRequestList.find(m => m.id!.toString() === sessionStorage.getItem('prevMergeRq')) ?? {title: 'empty'};
+        setFocusMerge(prevMergeRq);
+      }, [])
+
     return (
-        <div className='flex flex-col gap-2'>
-            {(mergeRequestList.length !== 0) ?
-                mergeRequestList.map(m => {
-                    return <div key={m.id} className='p-8 hover:outline flex pointer-events-auto'>
-                        {m.id} - {m.title} - {m.author!.username!}
-                    </div>
-                })
-                :
-                <div>Ingen Merge requests</div>}
+        <div className='w-full h-full bg-white rounded-md grid grid-cols-3 gap-2'>
+            <div className='col-span-2 flex flex-col gap-2 w-full p-2'>
+                {(mergeRequestList.length !== 0) ?
+                    mergeRequestList.map(m => {
+                        return <MergeRQListTile 
+                        merge={m} 
+                        handleClick={(fm) => {
+                            setFocusMerge(fm);
+                            sessionStorage.setItem('prevMergeRq', fm.id!.toString());
+                        }} 
+                        />
+                    })
+                    :
+                    <div>Ingen merge-requests</div>}
+            </div>
+            <div className='w-full h-full p-2'>
+                <MergeRQDetails merge={focusMerge} />
+            </div>
         </div>
     )
 }
