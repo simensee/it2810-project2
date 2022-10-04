ae05021cf93d3f0a21c3aad5a8311b5f43270134
piaasbjornsen <pia.asbjornsen@icloud.com>
2022-10-04 10:48:16 +0200

    More setup

---
+++ src/__tests__/users.test.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../App";
+import renderer from "react-test-renderer";
+
+ it("First snapshot test",()=>{
+    // Arrange 
+    
+    // Act
+
+    // Assert
+    
+ });
