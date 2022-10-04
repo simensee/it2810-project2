ee8b45e2c4678ea47e7b604df98a9e0b2161f676
brageher <brageher@stud.ntnu.no>
2022-10-04 19:37:44 +0200

    feat: implement session storage

--- src/Components/ProgressPageComponents/CommitList.tsx
+++ src/Components/ProgressPageComponents/CommitList.tsx
@@ -12,28 +12,29 @@ const CommitList = ({ commitList }: CommitListProps) => {
   const [focusCommit, setFocusCommit] = useState<Commit>({ title: 'empty' });
 
   useEffect(() => {
-    const prevCommit: Commit = commitList.find(c => c.short_id === sessionStorage.getItem('prevCommit')) ?? {title: 'empty'};
+    const prevCommit: Commit = commitList.find(c => c.short_id === sessionStorage.getItem('prevCommit')) ?? { title: 'empty' };
     setFocusCommit(prevCommit);
   }, [])
 
   return (
-    <div className='w-full h-full bg-white rounded-md grid grid-cols-3 gap-2'>
+    <div className='w-full h-full bg-white rounded-md grid grid-cols-3 gap-1'>
       <div className='col-span-2 flex flex-col gap-2 w-full h-full p-2 overflow-y-scroll'>
         {(commitList.length !== 0) ?
           commitList.map(c => {
-            return <CommitListTile 
-            commit={c} 
-            selected={(c === focusCommit)}
-            handleClick={(fc) => {
-              setFocusCommit(fc);
-              sessionStorage.setItem('prevCommit', fc.short_id!);
-            }} 
+            return <CommitListTile
+              key={c.short_id}
+              commit={c}
+              selected={(c === focusCommit)}
+              handleClick={(fc) => {
+                setFocusCommit(fc);
+                sessionStorage.setItem('prevCommit', fc.short_id!);
+              }}
             />
           })
           :
           <div>Ingen commits</div>}
       </div>
-      <div className='w-full h-full p-2'>
+      <div className='w-full h-full py-2 pr-2'>
         <CommitListDetails commit={focusCommit} />
       </div>
     </div>
